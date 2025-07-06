0fc2ee13cbe131e1a1bdc8dd7ac462c9352bad94
behrmann97 <sbehrmannc@gmail.com>
2025-07-06 16:28:33 -0400

    movimiento camara

--- GameJam/Assets/Scripts/Chete/Camera_Car.cs
+++ GameJam/Assets/Scripts/Chete/Camera_Car.cs
@@ -6,25 +6,22 @@ using System.Collections;
 
 public class Camera_Car : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public static float TIME_BETWEEN_EVENTS = 5;
     public Transform LWindow;
     public Transform RWindow;
     public Transform CarCenter;
+    public Camera camera;
 
     public float vel;
 
     [NonSerialized] public Transform target;
     [NonSerialized] public bool isMoving = false;
     private bool goingToPos2 = true;
+    private float moveTimer = 0;
 
     //public InputActionAsset inputActions;
     //public InputAction inputActionCamera;
     //public float cameraDirectionInput;
-
-    void Start()
-    {        
-        StartCoroutine(Sequence());
-    }
 
     // Update is called once per frame
     void Update()
@@ -33,38 +30,54 @@ public class Camera_Car : MonoBehaviour
         {
             GoToEvent();
         }
+        else
+        {
+            moveTimer += Time.deltaTime;
+            if (moveTimer >= TIME_BETWEEN_EVENTS)
+            {
+                NewEvent(getNextTarget());
+                moveTimer = 0;
+            }
+        }
     }
-    
+
+    void Start()
+    {
+        moveTimer = TIME_BETWEEN_EVENTS;
+    }
+
+    private Transform getNextTarget()
+    {
+        if (target == LWindow)
+        {
+            int index = UnityEngine.Random.Range(0, 2);
+            return index == 0 ? CarCenter : RWindow;
+        }
+        else
+        {
+            return LWindow;
+        }
+    }
+
     void GoToEvent()
     {
-        Debug.Log("Moviéndose hacia el objetivo...");
         transform.position = Vector3.MoveTowards(transform.position, target.position, vel * Time.deltaTime);
 
-        if (Vector3.Distance(transform.position, target.position) < 0.01f)
+        if (Vector3.Distance(transform.position, target.position) == 0)
         {
             isMoving = false;
+            if (target == LWindow) GameManager.Instance.gamePlayMode = GamePlayMode.PLAYING;
         }
-        
     }
+
     void NewEvent(Transform newTarget)
     {
         Debug.Log("Nuevo target");
         target = newTarget;
         isMoving = true;
+        if (target != LWindow) GameManager.Instance.gamePlayMode = GamePlayMode.IN_EVENT;
     }
-    IEnumerator Sequence()
-    {
-        for (int i = 0; i < 3; i++)
-        {
-            NewEvent(LWindow);
-            yield return new WaitUntil(() => !isMoving);
-            yield return new WaitForSeconds(5f);
 
-            NewEvent(RWindow);
-            yield return new WaitUntil(() => !isMoving);
-            yield return new WaitForSeconds(5f);
-        }
-    }
     //private void OnEnable()
     //{
     //    InputActionMap inputActionsMap = inputActions.FindActionMap("Player", throwIfNotFound: true);
--- GameJam/Assets/Scripts/GameManager.cs
+++ GameJam/Assets/Scripts/GameManager.cs
@@ -1,30 +1,66 @@
+using System;
 using UnityEngine;
+
+public enum GamePlayMode
+{
+    PLAYING,
+    IN_EVENT
+}
 
 public class GameManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-
+    public static GameManager Instance;
+    public static float IN_GAME_CAMERA_SIZE = 3.5f;
+    public static float IN_EVENT_CAMERA_SIZE = 6;
     public Camera_Car Cam;
 
-    public GameObject carBackground;
+    [NonSerialized] public GamePlayMode gamePlayMode;
 
-    bool gameplayRotation;
+    void Awake()
+    {
+        Instance = this;
+    }
+
     void Start()
     {
-        gameplayRotation = false;
+        gamePlayMode = GamePlayMode.IN_EVENT;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!Cam.isMoving)
+        if (gamePlayMode == GamePlayMode.PLAYING)
         {
-            if (Cam.transform.position == Cam.LWindow.transform.position && !gameplayRotation) 
+            if (Cam.transform.localPosition.y != 1)
             {
-                gameplayRotation = true;
-                carBackground.transform.Rotate(0, 0, 5);
+                Cam.transform.localPosition =
+                Vector3.MoveTowards(Cam.transform.localPosition, new Vector3(Cam.transform.position.x, 1, Cam.transform.position.z), Time.deltaTime);
             }
-                 
+            if (Cam.camera.orthographicSize > IN_GAME_CAMERA_SIZE)
+            {
+                Cam.camera.orthographicSize -= Time.deltaTime;
+            }
+            else
+            {
+                Cam.camera.orthographicSize = IN_GAME_CAMERA_SIZE;
+            }
+        }
+        else if (gamePlayMode == GamePlayMode.IN_EVENT)
+        {
+            if (Cam.transform.localPosition.y != 0)
+            {
+                Cam.transform.localPosition =
+                Vector3.MoveTowards(Cam.transform.localPosition, new Vector3(Cam.transform.position.x, 0, Cam.transform.position.z), Time.deltaTime);
+            }
+            if (Cam.camera.orthographicSize < IN_EVENT_CAMERA_SIZE)
+            {
+                Cam.camera.orthographicSize += Time.deltaTime;
+            }
+            else
+            {
+                Cam.camera.orthographicSize = IN_EVENT_CAMERA_SIZE;
+            }
         }
     }
 }
